ab128e37b34f9ba1f1b9347cb664658294de6f74
hamzaelha01 <35404886+hamzaelha01@users.noreply.github.com>
2018-03-06 20:43:13 +0000

    Update getcateg.php

--- STANDARD/assets/php/Admin/Produit/script/getcateg.php
+++ STANDARD/assets/php/Admin/Produit/script/getcateg.php
@@ -2,7 +2,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 $conn = mysqli_connect("localhost","root","root","Pressing");
-$output = array();
+$output = [];
 // $query  = "SELECT * FROM users";
 // $query  ="SELECT COMMANDE.ID_COMMANDE , COMMANDE.NBR_ARTICLES ,COMMANDE.LIVREUR_COLLECTE, COMMANDE.DD_COMMANDE , COMMANDE.DF_COMMANDE
 // FROM COMMANDE
@@ -10,6 +10,8 @@ $output = array();
 $query ="SELECT * FROM CATEGORIE";
 $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
+  
+  echo "good to go";
 while ($row = mysqli_fetch_array($result)) {
 $output[] = $row;
 }
