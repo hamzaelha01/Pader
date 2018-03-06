741c83bbb0c50d078c755f0959bad4015e54ba1b
hamzaelha01 <35404886+hamzaelha01@users.noreply.github.com>
2018-03-06 20:50:44 +0000

    go

--- STANDARD/assets/php/Admin/Produit/script/getcateg.php
+++ STANDARD/assets/php/Admin/Produit/script/getcateg.php
@@ -1,15 +1,23 @@
-<?php  
+<?php
 header('Access-Control-Allow-Origin: *');
-
- //load_categories.php  
- $connect = mysqli_connect("localhost", "root", "root", "Pressing");  
- $output = array();  
- $query = "SELECT * FROM CATEGORIE ";  
- $result = mysqli_query($connect, $query);  
- while($row = mysqli_fetch_array($result))  
- {  
-      $output[] = $row;  
- }  
- echo json_encode($output);  
-// return json_encode(['categories' => $output]);
- ?> 
+$conn = mysqli_connect("localhost","root","root","Pressing");
+$output = array();
+// $query  = "SELECT * FROM users";
+// $query  ="SELECT COMMANDE.ID_COMMANDE , COMMANDE.NBR_ARTICLES ,COMMANDE.LIVREUR_COLLECTE, COMMANDE.DD_COMMANDE , COMMANDE.DF_COMMANDE
+// FROM COMMANDE
+//   WHERE COMMANDE.STATUS  LIKE '%CONFIRME%'";
+$query ="SELECT * FROM CATEGORIE ";
+$result = mysqli_query($conn, $query);
+if (mysqli_num_rows($result) > 0) {
+ 
+ echo "good to go!";
+while ($row = mysqli_fetch_array($result)) {
+$output[] = $row;
+}
+ else 
+ {
+   echo " not good to go!";
+ }
+}
+echo json_encode($output);
+?> 
