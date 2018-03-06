51e330457ac98fa7f1c388536bcb8b25b47db278
PAD <mac@macs-MacBook-Pro.local>
2018-03-06 21:15:08 +0000

    go

--- STANDARD/assets/php/Admin/Produit/script/getcateg.php
+++ STANDARD/assets/php/Admin/Produit/script/getcateg.php
@@ -1,13 +1,22 @@
 <?php  
+
+
+
+require __DIR__ . '/library.php';
+
+$produit = new Produit();
+
+echo $produit->getCat();
+
  //load_categories.php  
- $connect = mysqli_connect("localhost", "root", "root", "Pressing");  
- $output = array();  
- $query = "SELECT  CATEGORIE.ID_CATEGORIE, CATEGORIE.DESGINATION_CAT FROM CATEGORIE ";  
- $result = mysqli_query($connect, $query);  
- while($row = mysqli_fetch_array($result))  
- {  
-      $output[] = $row;  
- }  
- echo json_encode($output);  
+//  $connect = mysqli_connect("localhost", "root", "root", "Pressing");  
+//  $output = array();  
+//  $query = "SELECT DISTINCT ID_CATEGORIE, DESGINATION_CAT FROM CATEGORIE ";  
+//  $result = mysqli_query($connect, $query);  
+//  while($row = mysqli_fetch_array($result))  
+//  {  
+//       $output[] = $row;  
+//  }  
+//  echo json_encode($output);  
 // return json_encode(['categories' => $output]);
  ?> 
