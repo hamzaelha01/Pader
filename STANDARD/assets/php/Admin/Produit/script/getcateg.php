5aeb60e83b028125a44969e3e3c2f38d87e6cbfa
hamzaelha01 <35404886+hamzaelha01@users.noreply.github.com>
2018-03-06 21:09:06 +0000

    Update getcateg.php

--- STANDARD/assets/php/Admin/Produit/script/getcateg.php
+++ STANDARD/assets/php/Admin/Produit/script/getcateg.php
@@ -1,17 +1,22 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-$conn = mysqli_connect("localhost","root","root","Pressing");
-$output = array();
-// $query  = "SELECT * FROM users";
-// $query  ="SELECT COMMANDE.ID_COMMANDE , COMMANDE.NBR_ARTICLES ,COMMANDE.LIVREUR_COLLECTE, COMMANDE.DD_COMMANDE , COMMANDE.DF_COMMANDE
-// FROM COMMANDE
-//   WHERE COMMANDE.STATUS  LIKE '%CONFIRME%'";
-$query ="SELECT * FROM CATEGORIE ";
-$result = mysqli_query($conn, $query);
-if (mysqli_num_rows($result) > 0) {
-while ($row = mysqli_fetch_array($result)) {
-    $output[] = $row;
-}
-}
-echo json_encode($output);
-?> 
+<?php  
+
+
+
+require __DIR__ . '/library.php';
+
+$produit = new Produit();
+
+echo $produit->getCat();
+
+ //load_categories.php  
+//  $connect = mysqli_connect("localhost", "root", "root", "Pressing");  
+//  $output = array();  
+//  $query = "SELECT DISTINCT ID_CATEGORIE, DESGINATION_CAT FROM CATEGORIE ";  
+//  $result = mysqli_query($connect, $query);  
+//  while($row = mysqli_fetch_array($result))  
+//  {  
+//       $output[] = $row;  
+//  }  
+//  echo json_encode($output);  
+// return json_encode(['categories' => $output]);
+ ?> 
