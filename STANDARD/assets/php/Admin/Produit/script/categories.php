ed4c66e409fd5472f942a18dcc79df7d5997fa3b
PAD <mac@macs-MacBook-Pro.local>
2018-03-06 21:19:26 +0000

    goc

--- STANDARD/assets/php/Admin/Produit/script/categories.php
+++ STANDARD/assets/php/Admin/Produit/script/categories.php
@@ -2,7 +2,7 @@
  //load_categories.php  
  $connect = mysqli_connect("localhost", "root", "root", "Pressing");  
  $output = array();  
- $query = "SELECT DISTINCT ID_CATEGORIE, DESIGNATION_CAT FROM CATEGORIE";  
+ $query = "SELECT DISTINCT ID_CATEGORIE, DESGINATION_CAT FROM CATEGORIE";  
  $result = mysqli_query($connect, $query);  
  while($row = mysqli_fetch_array($result))  
  {  
