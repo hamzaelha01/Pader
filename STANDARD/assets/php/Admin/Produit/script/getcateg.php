2360cf8ceae708d6b1f026bc0d838e079a3c2334
hamzaelha01 <35404886+hamzaelha01@users.noreply.github.com>
2018-03-06 20:44:18 +0000

    Update getcateg.php

--- STANDARD/assets/php/Admin/Produit/script/getcateg.php
+++ STANDARD/assets/php/Admin/Produit/script/getcateg.php
@@ -1,24 +1,15 @@
-
-<?php
+<?php  
 header('Access-Control-Allow-Origin: *');
-$conn = mysqli_connect("localhost","root","root","Pressing");
-$output = [];
-// $query  = "SELECT * FROM users";
-// $query  ="SELECT COMMANDE.ID_COMMANDE , COMMANDE.NBR_ARTICLES ,COMMANDE.LIVREUR_COLLECTE, COMMANDE.DD_COMMANDE , COMMANDE.DF_COMMANDE
-// FROM COMMANDE
-//   WHERE COMMANDE.STATUS  LIKE '%CONFIRME%'";
-$query ="SELECT * FROM CATEGORIE";
-$result = mysqli_query($conn, $query);
-if (mysqli_num_rows($result) > 0) {
-  
-  echo "good to go";
-while ($row = mysqli_fetch_array($result)) {
-$output[] = $row;
-}
-}
-else 
-{
-  echo "doesnt exist";
-}
-echo json_encode($output);
-?> 
+
+ //load_categories.php  
+ $connect = mysqli_connect("localhost", "root", "", "Pressing");  
+ $output = array();  
+ $query = "SELECT DISTINCT ID_CATEGORIE, DESGINATION_CAT FROM CATEGORIE ";  
+ $result = mysqli_query($connect, $query);  
+ while($row = mysqli_fetch_array($result))  
+ {  
+      $output[] = $row;  
+ }  
+ echo json_encode($output);  
+// return json_encode(['categories' => $output]);
+ ?> 
