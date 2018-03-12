d0f6d08d39bca92ae610a36551b656bb454a7d64
PAD <mac@macs-MacBook-Pro.local>
2018-03-12 18:02:52 +0000

    great

--- STANDARD/assets/php/Client /getCommandes.php
+++ STANDARD/assets/php/Client /getCommandes.php
@@ -11,7 +11,7 @@ header('Access-Control-Allow-Origin: *');
 $response= [];
 if(count($_POST)>0) {
 $conn = mysqli_connect("localhost","root","root","Pressing");
-	$result = mysqli_query($conn,"SELECT DISTINCT COMMANDE.ID_COMMANDE , DATE_FORMAT(COMMANDE.DD_COMMANDE, '%Y-%m-%d %H:%i') as DATECOMMANDE  , COMMANDE.STATUS ,COMMANDE.NBR_ARTICLES , COMMANDE.DF_COMMANDE FROM COMMANDE WHERE COMMANDE.STATUS IN ('EN ATTENTE','CONFIRME','TO COLLECT') , COMMANDE.STATUS ,COMMANDE.NBR_ARTICLES , COMMANDE.DF_COMMANDE FROM COMMANDE WHERE COMMANDE.STATUS IN ('EN ATTENTE','CONFIRME','TO COLLECT') AND COMMANDE.ID_CLIENT = ".$_POST["Iduser"]."");
+	$result = mysqli_query($conn,"SELECT DISTINCT COMMANDE.ID_COMMANDE , DATE_FORMAT(COMMANDE.DD_COMMANDE, '%Y-%m-%d %H:%i') as DATECOMMANDE , COMMANDE.STATUS ,COMMANDE.NBR_ARTICLES , COMMANDE.DF_COMMANDE FROM COMMANDE WHERE COMMANDE.STATUS IN ('EN ATTENTE','CONFIRME','TO COLLECT') AND COMMANDE.ID_CLIENT = ".$_POST["Iduser"]."");
     if(mysqli_num_rows($result)>0){
     while($row = $result->fetch_assoc()){
          // $response['status']= "loggedin";   
