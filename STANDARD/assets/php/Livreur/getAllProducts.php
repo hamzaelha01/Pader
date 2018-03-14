326733a0ce626c08c3c8fb5fbe85423c39b57606
PAD <mac@macs-MacBook-Pro.local>
2018-03-14 21:58:44 +0000

    onlydb

--- STANDARD/assets/php/Livreur/getAllProducts.php
+++ STANDARD/assets/php/Livreur/getAllProducts.php
@@ -3,9 +3,7 @@ header('Access-Control-Allow-Origin: *');
 $conn = mysqli_connect("localhost","root","root","Pressing");
 $output = array();
 // $query  = "SELECT * FROM users";
-$query  ="SELECT *
-FROM PRODUIT , CATEGORIE
-WHERE PRODUIT.ID_CATEGORIE = CATEGORIE.ID_CATEGORIE";
+$query  ="SELECT * FROM PRODUIT , CATEGORIE WHERE PRODUIT.ID_CATEGORIE = CATEGORIE.ID_CATEGORIE ORDER BY  CATEGORIE.DESGINATION_CAT DESC";
 $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
 while ($row = mysqli_fetch_array($result)) {
